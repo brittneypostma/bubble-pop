6f915679a2d7305055f9ccc5495566d5bd972b09
brittneypostma <bdesignwebdev@gmail.com>
2022-06-05 07:55:44 -0400

    finished

--- src/lib/components/Inputs.svelte
+++ src/lib/components/Inputs.svelte
@@ -1,11 +1,25 @@
 <script>
-	import { finalScore } from '../stores';
+	import { finalScore, totalBubbles } from '../stores';
 </script>
 
 <aside class="glass">
+	<h2>Game Settings</h2>
+
 	<label>
 		Winning Score:
 		<input class="glass" min="1" max="100" type="number" name="score" bind:value={$finalScore} />
+	</label>
+
+	<label>
+		Total Bubbles:
+		<input
+			class="glass"
+			min="1"
+			max="100"
+			type="number"
+			name="bubbles"
+			bind:value={$totalBubbles}
+		/>
 	</label>
 </aside>
 
@@ -15,6 +29,10 @@
 		position: absolute;
 		top: 0;
 		left: 0;
+		justify-items: flex-start;
+	}
+	aside > h2 {
+		border-bottom: 1px solid var(--white);
 	}
 	label {
 		display: flex;
