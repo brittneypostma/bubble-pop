001c13ca19bfda9fd5b9f011dcfd005c61c2c537
brittneypostma <bdesignwebdev@gmail.com>
2022-06-06 14:00:37 -0400

    starter files

--- src/App.svelte
+++ src/App.svelte
@@ -1,34 +1,5 @@
-<script>
-	import './lib/styles.css';
-	import Bubbles from './lib/components/bubbles/Bubbles.svelte';
-	import GameBar from './lib/components/GameBar.svelte';
-	import Inputs from './lib/components/Inputs.svelte';
-	import Footer from './lib/components/Footer.svelte';
-	import { score, session, lastScore } from './lib/stores';
-	import { onMount } from 'svelte/internal';
-	onMount(() => {
-		session.set(false);
-	});
-
-	function startGame() {
-		score.set(0);
-		session.set(true);
-	}
-</script>
-
 <main>
-	{#if !$session}
-		<Inputs />
-		<h1>Bubble Pop</h1>
-		<button on:click={startGame}>Start Popping</button>
-		{#if $lastScore !== 'null' || $lastScore !== null}
-			<h2>Previous High Score: {$lastScore}</h2>
-		{/if}
-		<Footer />
-	{:else if $session}
-		<Bubbles />
-		<GameBar />
-	{/if}
+	<h1>Welcome to Bubble Pop</h1>
 </main>
 
 <style>
